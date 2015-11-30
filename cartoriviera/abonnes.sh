64e97b819ba1056f9ed2085a259718e2aef54efe
3nids <denis.rouzaud@gmail.com>
2015-11-30 10:51:51 +0100

    fix new server config at vevey

--- cartoriviera/abonnes.sh
+++ cartoriviera/abonnes.sh
@@ -4,6 +4,6 @@
 set -e
 
 # to show progress, add: --progress
-rsync -r -t -v --delete --size-only -u -s /home/sige/mount/dao_saisie/ABONNES/CROQUIS drouzaud@cartoriviera3.vevey.ch:/var/www/www.cartoriviera.ch/htdocs/sige/abonnes/
-rsync -r -t -v --delete --size-only -u -s /home/sige/mount/dao_saisie/ABONNES/PHOTOS_ABTS drouzaud@cartoriviera3.vevey.ch:/var/www/www.cartoriviera.ch/htdocs/sige/abonnes/
+rsync -r -t -v --delete --size-only -u -s /home/sige/mount/dao_saisie/ABONNES/CROQUIS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
+rsync -r -t -v --delete --size-only -u -s /home/sige/mount/dao_saisie/ABONNES/PHOTOS_ABTS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
 #chmod -R 755 /home/sige/mount/cartoriviera/abonnes
