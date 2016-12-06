260a404811dbd126db8fb3f62c22f8b1b1336a01
3nids <denis.rouzaud@gmail.com>
2016-12-06 11:00:47 +0100

    fix ogr command

--- cartoriviera/export_qwat.sh
+++ cartoriviera/export_qwat.sh
@@ -21,25 +21,25 @@ ogr2ogr -sql "SELECT * FROM cadastre.enquete_camac"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln enquete_camac -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbcommun' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 ogr2ogr -sql "SELECT * FROM cadastre.enquete_cadastre"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln enquete_cadastre -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbcommun' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 ogr2ogr -sql "SELECT * FROM cadastre.vw_servitude_ligne"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln servitude_ligne -nlt MULTILINESTRING -progress -preserve_fid \
 PG:"dbname='$dbcommun' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 ogr2ogr -sql "SELECT * FROM cadastre.vw_servitude_polygon"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln servitude_poylgon -nlt MULTIPOLYGON -progress -preserve_fid \
 PG:"dbname='$dbcommun' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 ##########################################################################
 # QWAT
@@ -49,74 +49,78 @@ ogr2ogr -sql "SELECT * FROM qwat_od.vw_search_view
 UNION 
 SELECT *
 FROM dblink('dbname=sige_commun',\$\$SELECT 'Adresse Valais' AS layer_name, rue || ' ' || numero || ', ' || commune AS search_text, geometry FROM adresse.geopost WHERE commune = 'Bouveret' OR commune = 'Les Evouettes'\$\$)
-AS addr(layer_name varchar(120), search_text varchar(120), geometry geometry(Point,21781))" \
+AS addr(layer_name varchar(120), search_text varchar(120), geometry geometry(Point,21781))
+UNION 
+SELECT *
+FROM dblink('dbname=sige_commun',\$\$SELECT 'Cadastre Valais' AS layer_name, 'Parcelle ' || numero || ', Port-Valais' AS search_text, ST_Centroid(geom) AS geometry FROM cadastre.portvalais_bienfonds\$\$)
+AS parcel(layer_name varchar(120), search_text varchar(120), geometry geometry(Point,21781))" \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln search_view -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "pipes"
 ogr2ogr -sql "SELECT material_short_fr||' '||material_diameter AS _label, COALESCE(schema_force_visible, function_schema_visible) IS TRUE AS _schema_view, * FROM qwat_od.vw_export_pipe"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln pipe -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "pipe schema"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_pipe_schema"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln pipe_schema -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "nodes"
 ogr2ogr -sql "SELECT * FROM qwat_od.node "  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln node -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "parts"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_export_part "  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln part -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "valves"
 ogr2ogr -sql "SELECT *, COALESCE(valve_type_short_fr,'')||COALESCE(identification,'')||COALESCE(valve_function_short_fr,'') AS _label FROM qwat_od.vw_export_valve "  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln valve -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "hydrants"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_export_hydrant WHERE fk_distributor = 1"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln hydrant -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "pressure zones"
 ogr2ogr -sql "SELECT * FROM qwat_od.pressurezone" \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln pressurezone -nlt POLYGON -progress \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "print maps"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_printmap" \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln printmap -nlt MULTIPOLYGON -progress \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "meter"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_export_meter"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln meter -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "subscriber"
 ogr2ogr -sql "SELECT vw_export_subscriber.*, '<a href=javascript:app.openInfoWindow(\"https://www.cartoriviera.ch/sige/www/gallery.php?type=abonne&abonne='
@@ -126,7 +130,7 @@ FROM qwat_od.vw_export_subscriber"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln subscriber -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "subscriber_reference"
 ogr2ogr -sql "SELECT subscriber_reference.id, vw_export_subscriber.identification, subscriber_reference.geometry::geometry(Point,21781)
@@ -135,28 +139,28 @@ ON vw_export_subscriber.id = subscriber_reference.fk_subscriber;" \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln subscriber_reference -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "protections zone"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_protectionzone"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln protectionzone -nlt MULTIPOLYGON -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "leaks"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_export_leak"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln leak -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "telecommande"
 ogr2ogr -sql "SELECT * FROM qwat_od.vw_remote"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln telecommande -nlt MULTILINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 ##########################################################################
 # INSTALLATION
@@ -167,7 +171,7 @@ ogr2ogr -sql "SELECT *, '<a href=javascript:app.openInfoWindow(\"https://www.car
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln installation -nlt POINT -progress \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 
 
@@ -179,26 +183,26 @@ ogr2ogr -sql "SELECT * FROM qwat_dr.dimension_distance"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln dimension_distance -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 echo "dimensions orientation"
 ogr2ogr -sql "SELECT * FROM qwat_dr.dimension_orientation"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln dimension_orientation -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 echo "annotation point"
 ogr2ogr -sql "SELECT * FROM qwat_dr.annotationpoint"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln annotationpoint -nlt POINT -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 echo "annotation ligne"
 ogr2ogr -sql "SELECT * FROM qwat_dr.annotationline"  \
 -overwrite -a_srs EPSG:21781 -f SQLite $sqliteoutput \
 -nln annotationline -nlt LINESTRING -progress -preserve_fid \
 PG:"dbname='$dbqwat' host=$db_address port='5432' user='sige'" \
--dsco SPATIALITE=no -lco "SPATIAL_INDEX=no FORMAT=SPATIALITE" -gt 65536
+-dsco SPATIALITE=no -lco "SPATIAL_INDEX=no" -lco "FORMAT=SPATIALITE" -gt 65536
 
 
 
