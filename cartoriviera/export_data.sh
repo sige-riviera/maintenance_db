51db657d1a4f92829d8736b63c3135cd7244c35a
3nids <denis.rouzaud@gmail.com>
2017-08-09 16:14:28 +0200

    more fixes

--- cartoriviera/export_data.sh
+++ cartoriviera/export_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-/home/sige/maintenance_db/cartoriviera/ouvrages_copydata.sh
+/home/sige/maintenance_db/cartoriviera/ouvrages.sh
 
 /home/sige/maintenance_db/cartoriviera/abonnes.sh
--- cartoriviera/ouvrages.sh
+++ cartoriviera/ouvrages.sh
@@ -12,12 +12,11 @@ mkdir /home/sige/data/carto/data_ouvrage
 # disque monté pour dao_saisie
 disk_path=/home/sige/mount/ouvrages
 directories=( 01_RESERVOIRS 02_SOURCES 03_CHAMBRES 04_STATION_DE_POMPAGE)
-directories=( 02_SOURCES 03_CHAMBRES 04_STATION_DE_POMPAGE)
 for directory in "${directories[@]}"; do
   for ouvrage_path in $disk_path/$directory/*/; do
     num=$(sed -r 's/^.*_([0-9]{4})\//\1/' <<< $ouvrage_path)
     if [[ ! $num =~ ^[0-9]+ ]]; then
-      echo " * Not found in $ouvrage_path"
+      echo " * Number not found in $ouvrage_path" 1>&2 
       continue
     fi
     echo "Ouvrage $num"
@@ -30,12 +29,12 @@ for directory in "${directories[@]}"; do
     # copy files
     echo " * Copying pictures ..."
     if [[ ! -d $ouvrage_path/${num}_00_ETAT_ACTUEL/Photos/ ]]; then
-      echo "ERROR photos folder not found"
+      echo "ERROR photos folder not found in $ouvrage_path " 1>&2
     else
       find "$ouvrage_path/${num}_00_ETAT_ACTUEL/Photos/" -iregex '.*\(jpg\|png\)' -exec cp {} /home/sige/data/carto/data_ouvrage/$num/images/large/ \;
     fi
     if [[ ! -d $ouvrage_path/${num}_00_ETAT_ACTUEL/Plans/ ]]; then
-      echo "ERROR Plans folder not found"
+      echo "ERROR Plans folder not found in $ouvrage_path"  1>&2
     else
       find "$ouvrage_path/${num}_00_ETAT_ACTUEL/Plans/" -iregex '.*\(jpg\|png\)' -exec cp {} /home/sige/data/carto/data_ouvrage/$num/images/large/ \;
       find "$ouvrage_path/${num}_00_ETAT_ACTUEL/Plans/" -iregex '.*\(pdf\)' -exec cp {} /home/sige/data/carto/data_ouvrage/$num/pdf/ \;	
@@ -45,16 +44,15 @@ for directory in "${directories[@]}"; do
     files=(/home/sige/data/carto/data_ouvrage/$num/images/large/*)
     echo " * Resizing pictures ... ${#files[@]}"
     if [[ ${#files[@]} -gt 0 ]]; then
+	  mogrify -resize 2000x2000 -quality 92 /home/sige/data/carto/data_ouvrage/$num/images/large/*
 	  mogrify -resize 800x800 -quality 92 -path /home/sige/data/carto/data_ouvrage/$num/images/small/ /home/sige/data/carto/data_ouvrage/$num/images/large/*
 	fi
   done
 done
 
-exit
-
 
 
 # mettre à jour sur cartoriviera
 # to show progress, add: --progress
-rsync -r -t -v --delete --size-only --omit-dir-times -u -s /home/sige/data/carto/data_ouvrage/ drouzaud@cartoriviera3.vevey.ch:ouvrages/
+rsync -r -t -v --delete --size-only --omit-dir-times -u -s /home/sige/data/carto/data_ouvrage/ drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/ouvrages/
 
