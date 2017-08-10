ef61659c7d2f495c86ca21436da9eae2c418d9db
3nids <denis.rouzaud@gmail.com>
2017-08-10 10:02:07 +0200

    better scheduling of cron jobs

--- cartoriviera/abonnes.sh
+++ cartoriviera/abonnes.sh
@@ -4,6 +4,6 @@
 set -e
 
 # to show progress, add: --progress
-rsync -r -t -v --delete --size-only --omit-dir-times -u -s /home/sige/mount/abonnes/CROQUIS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
-rsync -r -t -v --delete --size-only --omit-dir-times -u -s /home/sige/mount/abonnes/PHOTOS_ABTS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
+rsync -r -t -v --delete --size-only --omit-dir-times --times -u -s /home/sige/mount/abonnes/CROQUIS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
+rsync -r -t -v --delete --size-only --omit-dir-times --times -u -s /home/sige/mount/abonnes/PHOTOS_ABTS drouzaud@cartoriviera3.vevey.ch:/var/www/vhosts/www.cartoriviera.ch/htdocs/sige/abonnes/
 #chmod -R 755 /home/sige/mount/cartoriviera/abonnes
--- cartoriviera/export_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-/home/sige/maintenance_db/cartoriviera/ouvrages.sh
-
-/home/sige/maintenance_db/cartoriviera/abonnes.sh
