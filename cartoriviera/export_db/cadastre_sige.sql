15c1ccd01bb4f610e3045996a94e2983ed05e2c4
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-11-20 09:12:57 -0400

    fix cadastre

--- cartoriviera/export_db/cadastre_sige.sql
+++ cartoriviera/export_db/cadastre_sige.sql
@@ -6,10 +6,10 @@ CREATE TABLE cartoriviera.sige_qgis_enquete_camac_mn95 AS SELECT * FROM cadastre
 CREATE TABLE cartoriviera.sige_qgis_servitude_ligne_mn95 AS SELECT * FROM cadastre.vw_servitude_ligne;
 CREATE TABLE cartoriviera.sige_qgis_servitude_zone_mn95 AS SELECT * FROM cadastre.vw_servitude_polygon;
 
-create table cartoriviera.sige_qgis_enquete as select * from sige_qgis_enquete_mn95;
-create table cartoriviera.sige_qgis_enquete_camac as select * from sige_qgis_enquete_camac_mn95;
-create table cartoriviera.sige_qgis_servitude_ligne as select * from sige_qgis_servitude_ligne_mn95;
-create table cartoriviera.sige_qgis_servitude_zone as select * from sige_qgis_servitude_zone_mn95;
+create table cartoriviera.sige_qgis_enquete as select * from cartoriviera.sige_qgis_enquete_mn95;
+create table cartoriviera.sige_qgis_enquete_camac as select * from cartoriviera.sige_qgis_enquete_camac_mn95;
+create table cartoriviera.sige_qgis_servitude_ligne as select * from cartoriviera.sige_qgis_servitude_ligne_mn95;
+create table cartoriviera.sige_qgis_servitude_zone as select * from cartoriviera.sige_qgis_servitude_zone_mn95;
 
 alter table cartoriviera.sige_qgis_enquete alter column geometry type geometry('linestring', 21781) using st_geomfromewkb(st_fineltra(geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
 alter table cartoriviera.sige_qgis_enquete_camac alter column geometry type geometry('point', 21781) using st_geomfromewkb(st_fineltra(geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
