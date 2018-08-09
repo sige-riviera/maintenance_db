a808b8b5697d0181c3c7dd41884702a978518d67
kandre <kevin.andre.gx@gmail.com>
2018-08-09 14:03:29 +0200

    fix typo export subscriber and meter references tables to cartoriviera

--- cartoriviera/export_db/qwat_meter_reference.sql
+++ cartoriviera/export_db/qwat_meter_reference.sql
@@ -10,6 +10,6 @@ select * from qwat_od.meter_reference;
 
 alter table cartoriviera.sige_qgis_qwat_meter_reference_mn95 alter column geometry type geometry('point', 2056) using st_force2d(geometry);
 
-create table cartoriviera.sige_qgis_qwat_meter_reference as select * from cartoriviera.sige_qgis_qwat_meter_reference;
+create table cartoriviera.sige_qgis_qwat_meter_reference as select * from cartoriviera.sige_qgis_qwat_meter_reference_mn95;
 
 alter table cartoriviera.sige_qgis_qwat_meter_reference alter column geometry type geometry('point', 21781) using st_geomfromewkb(st_fineltra(geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
--- cartoriviera/export_db/qwat_subscriber_reference.sql
+++ cartoriviera/export_db/qwat_subscriber_reference.sql
@@ -10,6 +10,6 @@ select * from qwat_od.subscriber_reference;
 
 alter table cartoriviera.sige_qgis_qwat_subscriber_reference_mn95 alter column geometry type geometry('point', 2056) using st_force2d(geometry);
 
-create table cartoriviera.sige_qgis_qwat_subscriber_reference as select * from cartoriviera.sige_qgis_qwat_subscriber_reference;
+create table cartoriviera.sige_qgis_qwat_subscriber_reference as select * from cartoriviera.sige_qgis_qwat_subscriber_reference_mn95;
 
 alter table cartoriviera.sige_qgis_qwat_subscriber_reference alter column geometry type geometry('point', 21781) using st_geomfromewkb(st_fineltra(geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
