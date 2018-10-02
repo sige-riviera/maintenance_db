f27bb390fe0c1d66ec1534ec6423e20ea922b9d9
kandre <kevin.andre.gx@gmail.com>
2018-10-02 15:00:58 +0200

    remove key duplicates from traduction script

--- cartoriviera/qgis_project/traduction.py
+++ cartoriviera/qgis_project/traduction.py
@@ -6,10 +6,15 @@ import codecs
 file = '{0}{1}'.format(QgsProject.instance().fileName(),'.trad')
 fo = codecs.open(file, 'w', 'utf-8')
 
+uniqueAliases = {}
 for layer in QgsMapLayerRegistry.instance().mapLayers().values():
-    aliases = layer.attributeAliases()
-    for field in aliases:
-        print u'"{0}": "{1}",'.format(field, aliases[field])
-        fo.write( u'"{0}": "{1}",\n'.format(field, aliases[field]))
+    layerAliases = layer.attributeAliases()
+    for key in layerAliases:
+        if key not in uniqueAliases:
+            uniqueAliases[key] = layerAliases[key]
+
+for field in uniqueAliases:
+    print u'"{0}": "{1}",'.format(field, uniqueAliases[field])
+    fo.write( u'"{0}": "{1}",\n'.format(field, uniqueAliases[field]))
 
 fo.close()
