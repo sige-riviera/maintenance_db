beeb4738923b0006faa5936cdc8db9fb708b9f32
kandre <kevin.andre.gx@gmail.com>
2019-02-14 11:38:59 +0100

    add disk_usage.sh

---
+++ disk_usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FULL=${1:-simple}
+
+# Exit on error
+set -e
+
+# inform on disk space available
+diskUsage=$(df -hT /home)
+echo "Info: disk usage dedicated to database backups on s2laveyre:" 1>&2
+echo "$diskUsage" 1>&2
+
