a398a01a60d802ba6dbaaf78e3b1fb6ffd2fb2d9
kandre <kevin.andre.gx@gmail.com>
2020-07-01 09:48:48 +0200

    update scripts

---
+++ data_maintenance2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e 
+
+export PGOPTIONS='--client-min-messages=error'
+
+# Update pressure zones attribute on pipes
+psql -d qwat -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
+
+# Update district attribute on nodes (suscriber, hydrant, etc.)
+psql -d qwat -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
