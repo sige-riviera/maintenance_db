d41d55248c6491213920a967974f557f6cea6a98
kandre <kevin.andre.gx@gmail.com>
2020-12-08 16:58:59 +0100

    update scripts

--- mount_drives.sh
+++ mount_drives.sh
@@ -5,10 +5,17 @@ PASS_BACKUP=`cat /home/sitadmin/sit/pass/backup`
 NAME_MOUNT=`cat /home/sitadmin/sit/pass/name_data_server`
 NAME_BACKUP=`cat /home/sitadmin/sit/pass/name_backup_server`
 
+mkdir /home/sitadmin/sit/mount/abonnes
+mkdir /home/sitadmin/sit/mount/reseau
+mkdir /home/sitadmin/sit/mount/ouvrages
+mkdir /home/sitadmin/sit/mount/comm_tech_ro
+mkdir /home/sitadmin/sit/mount/cartoriviera
+
 sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
 sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
 sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_MOUNT/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
+#TODO replace with new account (sitscripts) when ready :
+#sudo /bin/mount -t cifs //$NAME_MOUNT/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
 
 sudo /bin/mount -t cifs //$NAME_BACKUP/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=backupadmin,password=$PASS_BACKUP,dom=sige.ch,gid=1000,uid=1000
 
--- unmount_drives.sh
+++ unmount_drives.sh
@@ -2,22 +2,17 @@
 
 sudo /bin/umount /home/sitadmin/sit/mount/abonnes
 rm -rf /home/sitadmin/sit/mount/abonnes
-mkdir /home/sitadmin/sit/mount/abonnes
 
 sudo /bin/umount /home/sitadmin/sit/mount/reseau
 rm -rf /home/sitadmin/sit/mount/reseau
-mkdir /home/sitadmin/sit/mount/reseau
 
 sudo /bin/umount /home/sitadmin/sit/mount/ouvrages
 rm -rf /home/sitadmin/sit/mount/ouvrages
-mkdir /home/sitadmin/sit/mount/ouvrages
 
 sudo /bin/umount /home/sitadmin/sit/mount/comm_tech_ro
 rm -rf /home/sitadmin/sit/mount/comm_tech_ro
-mkdir /home/sitadmin/sit/mount/comm_tech_ro
 
 #sudo fusermount -u /home/sitadmin/sit/mount/cartoriviera
 sudo /bin/umount -l /home/sitadmin/sit/mount/cartoriviera
 rm -rf /home/sitadmin/sit/mount/cartoriviera
-mkdir /home/sitadmin/sit/mount/cartoriviera
 
