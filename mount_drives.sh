be2e3401d6ff90007eb29a747f4022bb71b60941
kandre <kevin.andre.gx@gmail.com>
2020-12-15 09:35:34 +0100

    update scripts

--- mount_drives.sh
+++ mount_drives.sh
@@ -8,14 +8,13 @@ NAME_BACKUP=`cat /home/sitadmin/sit/pass/name_backup_server`
 mkdir /home/sitadmin/sit/mount/abonnes
 mkdir /home/sitadmin/sit/mount/reseau
 mkdir /home/sitadmin/sit/mount/ouvrages
-#mkdir /home/sitadmin/sit/mount/comm_tech_ro TODO enable when sitscripts account ready
+mkdir /home/sitadmin/sit/mount/comm_tech_ro
 mkdir /home/sitadmin/sit/mount/cartoriviera
 
-sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
-#TODO replace with new account (sitscripts) when ready :
-#sudo /bin/mount -t cifs //$NAME_MOUNT/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=infor,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
+sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
+sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=sitscripts,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
+sudo /bin/mount -t cifs //$NAME_MOUNT/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=sitscripts,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
+sudo /bin/mount -t cifs //$NAME_MOUNT/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_MOUNT,dom=sige.ch,gid=1000,uid=1000
 
 sudo /bin/mount -t cifs //$NAME_BACKUP/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=backupadmin,password=$PASS_BACKUP,dom=sige.ch,gid=1000,uid=1000
 
