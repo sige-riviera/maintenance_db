3c4c3dd87bc63fc876cd395d3edc05048d87fad8
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2022-05-04 12:27:01 +0200

    config for new backup server

--- mount_drives.sh
+++ mount_drives.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Note: version for cifs must be correct depending on the machine
+
 NAME_DATA_SRV=`cat /home/sitadmin/sit/pass/name_data_server`
+NAME_BACKUP_SRV_OLD=`cat /home/sitadmin/sit/pass/name_backup_server_old`
 NAME_BACKUP_SRV=`cat /home/sitadmin/sit/pass/name_backup_server`
 
 PASS_DATA_SRV=`cat /home/sitadmin/sit/pass/pass_data_server`
@@ -12,7 +15,8 @@ mkdir /home/sitadmin/sit/mount/reseau
 mkdir /home/sitadmin/sit/mount/ouvrages
 mkdir /home/sitadmin/sit/mount/data_prod
 mkdir /home/sitadmin/sit/mount/comm_tech_ro
-mkdir /home/sitadmin/sit/mount//backup_sbk_pierrier
+mkdir /home/sitadmin/sit/mount/backup_sbk_pierrier
+mkdir /home/sitadmin/sit/mount/backup_server_nas
 mkdir /home/sitadmin/sit/mount/cartoriviera_secured
 
 sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
@@ -21,7 +25,8 @@ sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/OUVRAGES /h
 sudo /bin/mount -o rw -t cifs //$NAME_DATA_SRV/dao_saisie$/05_DONNEES_SIT/DATA_PROD/ /home/sitadmin/sit/mount/data_prod -o user=infor,password=$PASS_INFOR,dom=sige.ch,gid=1000,uid=1000
 sudo /bin/mount -t cifs //$NAME_DATA_SRV/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
 
-sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0
+sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0
+sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_server_nas/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=3.0 # Win 2012+
 
 #fusermount -u /home/sitadmin/sit/mount/cartoriviera_secured
 sshfs kandre@cartoriviera3.vevey.ch:/var/sig/files/private/sige /home/sitadmin/sit/mount/cartoriviera_secured # SSH connection to cartoriviera (password protected) infrastructure uses private and public keys ~/.ssh/id_rsa and ~/.ssh/id_rsa.pub
