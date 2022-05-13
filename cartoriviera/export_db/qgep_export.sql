e9df26cb28a41c8d3d00c35805167b3ea061e51b
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2022-05-13 08:32:54 +0200

    update export scripts to qgep 1.5.6 version

--- cartoriviera/export_db/qgep_export.sql
+++ cartoriviera/export_db/qgep_export.sql
@@ -4,7 +4,7 @@ BEGIN;
 DROP SCHEMA IF EXISTS usr_cartoriviera CASCADE;
 CREATE SCHEMA usr_cartoriviera;
 
-CREATE TABLE usr_cartoriviera.sige_qgis_qgep_reach_mn95 AS SELECT * FROM qgep_export.vw_export_reach;
-CREATE TABLE usr_cartoriviera.sige_qgis_qgep_wastewater_structure_mn95 AS SELECT * FROM qgep_export.vw_export_wastewater_structure;
+CREATE TABLE usr_cartoriviera.sige_qgis_qgep_reach_mn95 AS SELECT * FROM qgep_od.vw_export_reach;
+CREATE TABLE usr_cartoriviera.sige_qgis_qgep_wastewater_structure_mn95 AS SELECT * FROM qgep_od.vw_export_wastewater_structure;
 
 COMMIT;
