57ab4ef14db35d3795f857d32dfe9148a6522ded
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2022-05-17 15:44:58 +0200

    add script to auto copy qwat db for statistics

---
+++ archive_db_for_statistics.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#set -e 
+
+export PGOPTIONS='--client-min-messages=warning'
+
+currentDay=`date +'%d'`
+currentMonth=`date +'%m'`
+currentYear=`date +'%Y'`
+sourceDb=qwat_prod
+
+# Disconnect users to free databases (before that manually create a copy of prod db named qgep_prod_copy)
+psql -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$sourceDb';"
+
+# Create a archived database for statistics on 1st january
+if [[ $currentDay=01 && $currentMonth=01 ]]
+then
+  psql -d postgres -c "CREATE DATABASE qwat_prod_statistics_${currentYear}${currentMonth}${currentDay} WITH TEMPLATE $sourceDb;"
+fi
+
+# Redirect stdout to stderr
+if [[ $MAILSTDOUT = true ]]; then
+  echo "End of the script file." 1>&2
+fi
