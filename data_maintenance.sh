75a8516fe66db806ef9f7af11224642e96f29a5a
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2022-07-29 08:33:23 +0200

    use sige user instead of sitadmin server user because scripts are now executed with crontab as root

--- data_maintenance.sh
+++ data_maintenance.sh
@@ -4,9 +4,9 @@ set -e
 
 export PGOPTIONS='--client-min-messages=warning'
 
-psql -d qwat_prod -c "SELECT qwat_od.fn_update_pipe_crossing()"
+psql -U sige -d qwat_prod -c "SELECT qwat_od.fn_update_pipe_crossing()"
 
-psql -d qwat_prod -c "SELECT qwat_od.fn_node_set_type()"
+psql -U sige -d qwat_prod -c "SELECT qwat_od.fn_node_set_type()"
 
 # psql -d qwat_prod -c "REFRESH MATERIALIZED VIEW qwat_od.vw_pipe_schema_node;"
 
--- data_maintenance2.sh
+++ data_maintenance2.sh
@@ -5,10 +5,10 @@ set -e
 export PGOPTIONS='--client-min-messages=error'
 
 # Update pressure zones attribute on pipes
-psql -d qwat_prod -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
+psql -U sige -d qwat_prod -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
 
 # Update district attribute on nodes (suscriber, hydrant, etc.)
-psql -d qwat_prod -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
+psql -U sige -d qwat_prod -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
 
 # Redirect stdout to stderr
 if [[ $MAILSTDOUT = true ]]; then
