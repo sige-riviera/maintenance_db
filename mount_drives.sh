ec34bcae747ba695af37bb045da254d09e1b15e1
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2023-01-05 11:43:46 +0100

    improve mount script

--- mount_drives.sh
+++ mount_drives.sh
@@ -10,23 +10,24 @@ PASS_DATA_SRV=`cat /home/sitadmin/sit/pass/pass_data_server`
 PASS_BACKUP_SRV=`cat /home/sitadmin/sit/pass/pass_backup_server`
 PASS_INFOR=`cat /home/sitadmin/sit/pass/pass_infor`
 
-mkdir /home/sitadmin/sit/mount/abonnes
-mkdir /home/sitadmin/sit/mount/reseau
-mkdir /home/sitadmin/sit/mount/ouvrages
-mkdir /home/sitadmin/sit/mount/data_prod
-mkdir /home/sitadmin/sit/mount/comm_tech_ro
-mkdir /home/sitadmin/sit/mount/backup_sbk_pierrier
-mkdir /home/sitadmin/sit/mount/backup_server_nas
-mkdir /home/sitadmin/sit/mount/cartoriviera_secured
+for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_sbk_pierrier backup_server_nas
+do
+  mkdir /home/sitadmin/sit/mount/$internalMountName || echo "Cannot create folder $internalMountName, skipping the statement."
+done
 
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -o rw -t cifs //$NAME_DATA_SRV/dao_saisie$/05_DONNEES_SIT/DATA_PROD/ /home/sitadmin/sit/mount/data_prod -o user=infor,password=$PASS_INFOR,dom=sige.ch,gid=1000,uid=1000
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000
+for externalMountName in cartoriviera_secured
+do
+  mkdir /home/sitadmin/sit/mount/$externalMountName || echo "Cannot create folder $externalMountName, skipping the statement."
+done
 
-sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0
-sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_server_nas/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=3.0 # Win 2012+
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount abonnes folder"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount reseau reseau"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount ouvrages folder"
+sudo /bin/mount -t cifs -o rw //$NAME_DATA_SRV/dao_saisie$/05_DONNEES_SIT/DATA_PROD/ /home/sitadmin/sit/mount/data_prod -o user=infor,password=$PASS_INFOR,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount data_prod folder"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount comm_tech_ro folder"
+
+sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0 || echo "Cannot mount backup_sbk_pierrier folder"
+sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_server_nas/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=3.0 || echo "Cannot mount backup_server_nas folder" # vers=3.0 Win 2012+
 
 #fusermount -u /home/sitadmin/sit/mount/cartoriviera_secured
 sshfs kandre@cartoriviera3.vevey.ch:/var/sig/files/private/sige /home/sitadmin/sit/mount/cartoriviera_secured # SSH connection to cartoriviera (password protected) infrastructure uses private and public keys ~/.ssh/id_rsa and ~/.ssh/id_rsa.pub
--- unmount_drives.sh
+++ unmount_drives.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_sbk_pierrier
+for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_sbk_pierrier backup_server_nas
 do
   #ls -la /home/sitadmin/sit/mount/$externalMountName
   sudo /bin/umount /home/sitadmin/sit/mount/$internalMountName || echo "Cannot unmount mounting point, skipping the statement."
-  rm -rf /home/sitadmin/sit/mount/$internalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  sudo rm -rf /home/sitadmin/sit/mount/$internalMountName || echo "Cannot delete mounting point folder, skipping the statement."
 done
 
 for externalMountName in cartoriviera_secured
 do
   #sudo fusermount -u /home/sitadmin/sit/mount/$externalMountName || echo "Cannot unmount mounting point, skipping the statement."
   sudo /bin/umount -l /home/sitadmin/sit/mount/$externalMountName || echo "Cannot unmount mounting point, skipping the statement."
-  rm -rf /home/sitadmin/sit/mount/$externalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  sudo rm -rf /home/sitadmin/sit/mount/$externalMountName || echo "Cannot delete mounting point folder, skipping the statement."
 done
 
 
