81f0ef1ba52492ac972465fbaa2552668ac56af4
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2023-01-05 11:57:43 +0100

    remove old backup mount point

--- mount_drives.sh
+++ mount_drives.sh
@@ -10,7 +10,7 @@ PASS_DATA_SRV=`cat /home/sitadmin/sit/pass/pass_data_server`
 PASS_BACKUP_SRV=`cat /home/sitadmin/sit/pass/pass_backup_server`
 PASS_INFOR=`cat /home/sitadmin/sit/pass/pass_infor`
 
-for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_sbk_pierrier backup_server_nas
+for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_server_nas
 do
   mkdir /home/sitadmin/sit/mount/$internalMountName || echo "Cannot create folder $internalMountName, skipping the statement."
 done
@@ -26,7 +26,7 @@ sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/OUVRAGES /h
 sudo /bin/mount -t cifs -o rw //$NAME_DATA_SRV/dao_saisie$/05_DONNEES_SIT/DATA_PROD/ /home/sitadmin/sit/mount/data_prod -o user=infor,password=$PASS_INFOR,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount data_prod folder"
 sudo /bin/mount -t cifs //$NAME_DATA_SRV/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount comm_tech_ro folder"
 
-sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0 || echo "Cannot mount backup_sbk_pierrier folder"
+#sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0 || echo "Cannot mount backup_sbk_pierrier folder"
 sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_server_nas/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=3.0 || echo "Cannot mount backup_server_nas folder" # vers=3.0 Win 2012+
 
 #fusermount -u /home/sitadmin/sit/mount/cartoriviera_secured
--- unmount_drives.sh
+++ unmount_drives.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_sbk_pierrier backup_server_nas
+for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_server_nas
 do
   #ls -la /home/sitadmin/sit/mount/$externalMountName
   sudo /bin/umount /home/sitadmin/sit/mount/$internalMountName || echo "Cannot unmount mounting point, skipping the statement."
