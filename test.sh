b5dad43d1ed2ad98e9aac3fa2672fa47eaaeb1bc
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2023-01-27 12:26:07 +0100

    fix scripts

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Test regular database operation
-psql -d postgres -c "SELECT version();" 1>&2
-psql -d postgres -c "SELECT PostGIS_full_version();" 1>&2
+psql -U sige -d postgres -c "SELECT version();" 1>&2
+psql -U sige -d postgres -c "SELECT PostGIS_full_version();" 1>&2
 
 # Test stdout (1) / Redirect stdout to stderr
 if [[ $MAILSTDOUT = true ]]; then
