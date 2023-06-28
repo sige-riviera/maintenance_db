e69f1e849f33974b98ae6885f70c7e696c1fb2ff
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2023-06-28 16:26:20 +0200

    update pressure zone foreign keys on others objets

--- data_maintenance2.sh
+++ data_maintenance2.sh
@@ -6,6 +6,8 @@ export PGOPTIONS='--client-min-messages=error'
 
 # Update pressure zones attribute on pipes
 psql -U sige -d qwat_prod -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
+psql -U sige -d qwat_prod -c "update qwat_od.node set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
+psql -U sige -d qwat_prod -c "update qwat_od.valve set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
 
 # Update district attribute on nodes (suscriber, hydrant, etc.)
 psql -U sige -d qwat_prod -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
