037fabef4532ac25373ed6099f2a8729912825f4
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2023-06-30 08:17:07 +0200

    add pressure zones sync onto valves and nodes

--- data_maintenance2.sh
+++ data_maintenance2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-set -e 
+#set -e
 
 export PGOPTIONS='--client-min-messages=error'
 
-# Update pressure zones attribute on pipes
-psql -U sige -d qwat_prod -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
-psql -U sige -d qwat_prod -c "update qwat_od.node set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
-psql -U sige -d qwat_prod -c "update qwat_od.valve set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)"
+# Update pressure zones attribute on pipes, nodes and valves
+psql -U sige -d qwat_prod -c "update qwat_od.pipe set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)" 1>&2
+psql -U sige -d qwat_prod -c "update qwat_od.node set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)" 1>&2
+psql -U sige -d qwat_prod -c "update qwat_od.valve set fk_pressurezone = qwat_od.fn_get_pressurezone(geometry)" 1>&2
 
 # Update district attribute on nodes (suscriber, hydrant, etc.)
-psql -U sige -d qwat_prod -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
+#psql -U sige -d qwat_prod -c "update qwat_od.node set fk_district = qwat_od.fn_get_district(geometry)"
 
 # Redirect stdout to stderr
 if [[ $MAILSTDOUT = true ]]; then
