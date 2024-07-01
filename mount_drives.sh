aae806a949153ce111f7b136f95079927192bdf0
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2024-07-01 15:02:45 +0200

    adapt mounting points for new file system

--- mount_drives.sh
+++ mount_drives.sh
@@ -3,30 +3,27 @@
 # Note: version for cifs must be correct depending on the machine
 
 NAME_DATA_SRV=`cat /home/sitadmin/sit/pass/name_data_server`
-NAME_BACKUP_SRV_OLD=`cat /home/sitadmin/sit/pass/name_backup_server_old`
-NAME_BACKUP_SRV=`cat /home/sitadmin/sit/pass/name_backup_server`
-
 PASS_DATA_SRV=`cat /home/sitadmin/sit/pass/pass_data_server`
-PASS_BACKUP_SRV=`cat /home/sitadmin/sit/pass/pass_backup_server`
-PASS_INFOR=`cat /home/sitadmin/sit/pass/pass_infor`
 
-for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_server_nas
+NAME_BACKUP_SRV=`cat /home/sitadmin/sit/pass/name_backup_server`
+PASS_BACKUP_SRV=`cat /home/sitadmin/sit/pass/pass_backup_server`
+
+#for internalMountName in abonnes reseau ouvrages assainissement backup_server_nas
+for internalMountName in abonnes reseau ouvrages backup_server_nas #temporary until sitscripts get read-only access right onto N:\02_ASSAINISSEMENT\00_DOC_TECHNIQUE
 do
   mkdir /home/sitadmin/sit/mount/$internalMountName || echo "Cannot create folder $internalMountName, skipping the statement."
 done
 
-for externalMountName in cartoriviera_secured
+for externalMountName in cartoriviera_secured cartoriviera_unsecured
 do
   mkdir /home/sitadmin/sit/mount/$externalMountName || echo "Cannot create folder $externalMountName, skipping the statement."
 done
 
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount abonnes folder"
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/RESEAU /home/sitadmin/sit/mount/reseau -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount reseau reseau"
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/dao_saisie$/01_DISTRIBUTION/OUVRAGES /home/sitadmin/sit/mount/ouvrages -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount ouvrages folder"
-sudo /bin/mount -t cifs -o rw //$NAME_DATA_SRV/dao_saisie$/05_DONNEES_SIT/DATA_PROD/ /home/sitadmin/sit/mount/data_prod -o user=infor,password=$PASS_INFOR,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount data_prod folder"
-sudo /bin/mount -t cifs //$NAME_DATA_SRV/comm_tech_ro$ /home/sitadmin/sit/mount/comm_tech_ro -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount comm_tech_ro folder"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/EXPLOITATION/01_EAU_POTABLE/00_DOC_TECHNIQUE/71_ABONNES /home/sitadmin/sit/mount/abonnes -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount abonnes folder"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/EXPLOITATION/01_EAU_POTABLE/00_DOC_TECHNIQUE/01_RESEAU /home/sitadmin/sit/mount/reseau -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount reseau reseau"
+sudo /bin/mount -t cifs //$NAME_DATA_SRV/EXPLOITATION/01_EAU_POTABLE/00_DOC_TECHNIQUE /home/sitadmin/sit/mount/ouvrages -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount ouvrages folder"
+#sudo /bin/mount -t cifs //$NAME_DATA_SRV/EXPLOITATION/02_ASSAINISSEMENT/00_DOC_TECHNIQUE /home/sitadmin/sit/mount/assainissement -o user=sitscripts,password=$PASS_DATA_SRV,dom=sige.ch,gid=1000,uid=1000 || echo "Cannot mount assainissement folder" #temporary disabled until sitscripts get read-only access right onto N:\02_ASSAINISSEMENT\00_DOC_TECHNIQUE
 
-#sudo /bin/mount -t cifs //$NAME_BACKUP_SRV_OLD/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_sbk_pierrier/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=1.0 || echo "Cannot mount backup_sbk_pierrier folder"
 sudo /bin/mount -t cifs //$NAME_BACKUP_SRV/bk_laveyre/qgis /home/sitadmin/sit/mount/backup_server_nas/ -o user=intranet,password=$PASS_BACKUP_SRV,dom=sige.ch,gid=1000,uid=1000,vers=3.0 || echo "Cannot mount backup_server_nas folder" # vers=3.0 Win 2012+
 
 #fusermount -u /home/sitadmin/sit/mount/cartoriviera_secured
--- unmount_drives.sh
+++ unmount_drives.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-for internalMountName in abonnes reseau ouvrages data_prod comm_tech_ro backup_server_nas
+#for internalMountName in abonnes reseau ouvrages assainissement backup_server_nas
+for internalMountName in abonnes reseau ouvrages backup_server_nas #temporary until sitscripts get read-only access right onto N:\02_ASSAINISSEMENT\00_DOC_TECHNIQUE
 do
   #ls -la /home/sitadmin/sit/mount/$externalMountName
   sudo /bin/umount /home/sitadmin/sit/mount/$internalMountName || echo "Cannot unmount mounting point, skipping the statement."
-  sudo rm -rf /home/sitadmin/sit/mount/$internalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  #sudo rm -rf /home/sitadmin/sit/mount/$internalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  sudo rmdir /home/sitadmin/sit/mount/$internalMountName || echo "Cannot delete mounting point folder, skipping the statement."
 done
 
-for externalMountName in cartoriviera_secured
+for externalMountName in cartoriviera_secured cartoriviera_unsecured
 do
   #sudo fusermount -u /home/sitadmin/sit/mount/$externalMountName || echo "Cannot unmount mounting point, skipping the statement."
   sudo /bin/umount -l /home/sitadmin/sit/mount/$externalMountName || echo "Cannot unmount mounting point, skipping the statement."
-  sudo rm -rf /home/sitadmin/sit/mount/$externalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  #sudo rm -rf /home/sitadmin/sit/mount/$externalMountName || echo "Cannot delete mounting point folder, skipping the statement."
+  sudo rmdir /home/sitadmin/sit/mount/$externalMountName || echo "Cannot delete mounting point folder, skipping the statement."
 done
-
-
