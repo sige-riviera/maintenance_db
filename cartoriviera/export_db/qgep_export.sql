f04c42803b50613f95721e70ea0a37473ca97ac5
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2024-08-20 14:32:51 +0200

    Transform sige_qgis_qgep_reach_mn95 CompoundCurveZ to MultilineStringZ geometry for TinyOWS

--- cartoriviera/export_db/qgep_export.sql
+++ cartoriviera/export_db/qgep_export.sql
@@ -7,4 +7,9 @@ CREATE SCHEMA usr_cartoriviera;
 CREATE TABLE usr_cartoriviera.sige_qgis_qgep_reach_mn95 AS SELECT * FROM qgep_od.vw_export_reach;
 CREATE TABLE usr_cartoriviera.sige_qgis_qgep_wastewater_structure_mn95 AS SELECT * FROM qgep_od.vw_export_wastewater_structure;
 
+-- Transform sige_qgis_qgep_reach_mn95 CompoundCurveZ to MultilineStringZ geometry (TinyOWS for WFS layers for communities does not support CompoundCurveZ)
+ALTER TABLE usr_cartoriviera.sige_qgis_qgep_reach_mn95
+ALTER COLUMN progression_geometry TYPE geometry(MultiLineStringZ, 2056)
+USING ST_Multi(ST_CurveToLine(progression_geometry));
+
 COMMIT;
--- cartoriviera/export_db/qgep_transform.sql
+++ cartoriviera/export_db/qgep_transform.sql
@@ -4,7 +4,7 @@ BEGIN;
 create table usr_cartoriviera.sige_qgis_qgep_reach as select * from usr_cartoriviera.sige_qgis_qgep_reach_mn95;
 create table usr_cartoriviera.sige_qgis_qgep_wastewater_structure as select * from usr_cartoriviera.sige_qgis_qgep_wastewater_structure_mn95;
 
-ALTER TABLE usr_cartoriviera.sige_qgis_qgep_reach ALTER COLUMN progression_geometry TYPE Geometry(CompoundCurveZ, 21781) USING ST_GeomFromEWKB(ST_Fineltra(progression_geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
+ALTER TABLE usr_cartoriviera.sige_qgis_qgep_reach ALTER COLUMN progression_geometry TYPE Geometry(MultiLineStringZ, 21781) USING ST_GeomFromEWKB(ST_Fineltra(progression_geometry, 'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
 ALTER TABLE usr_cartoriviera.sige_qgis_qgep_wastewater_structure ALTER COLUMN situation_geometry TYPE Geometry(Point, 21781) USING ST_GeomFromEWKB(ST_Fineltra(situation_geometry,   'chenyx06.chenyx06_triangles', 'the_geom_lv95', 'the_geom_lv03'));
 
 COMMIT;
