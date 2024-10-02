cc43352701709cc6a61eb3cd3aad642c6db5e4f9
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2024-10-02 09:11:53 +0200

    update croquis reseau export to cartoriviera with new character case

--- controle_croquis_reseau.sh
+++ controle_croquis_reseau.sh
@@ -6,22 +6,21 @@ set -e
 
 FOLDERPATH=/home/sitadmin/sit/mount/reseau
 
-declare -A COMMUNES=( ["Attalens"]="Attalens" \
-                      ["82"]="Blonay" \
-                      ["84"]="Chardonne" \
-                      ["81"]="Corseaux" \
-                      ["80"]="Corsier" \
-                      ["88"]="Jongny" \
-                      ["60"]="La_Tour_de_Peilz" \
-                      ["51"]="Montreux" \
-                      ["43"]="Port-Valais" \
-                      ["Roche"]="Roche" \
-                      ["Remaufens"]="Remaufens" \
-                      ["83"]="St_Legier" \
-                      ["71"]="Vevey" \
-                      ["50"]="Veytaux" \
-                      ["37"]="Villeneuve-Noville" )
-
+declare -A COMMUNES=( ["Attalens"]="ATTALENS" \
+                      ["82"]="BLONAY" \
+                      ["84"]="CHARDONNE" \
+                      ["81"]="CORSEAUX" \
+                      ["80"]="CORSIER" \
+                      ["88"]="JONGNY" \
+                      ["60"]="LA_TOUR_DE_PEILZ" \
+                      ["51"]="MONTREUX" \
+                      ["43"]="PORT-VALAIS" \
+                      ["Roche"]="ROCHE" \
+                      ["Remaufens"]="REMAUFENS" \
+                      ["83"]="ST_LEGIER" \
+                      ["71"]="VEVEY" \
+                      ["50"]="VEYTAUX" \
+                      ["37"]="VILLENEUVE-NOVILLE" ) # Respect de la casse, même si Windows n'y est pas sensible
 
 FILE_NOT_FOUND=false
 for file in $(PGUSER=sige PGDATABASE=sige_commun psql -t -q -c "select file from distribution.croquis_reseau order by file asc"); do
@@ -32,7 +31,7 @@ for file in $(PGUSER=sige PGDATABASE=sige_commun psql -t -q -c "select file from
     if [[ $FILE_NOT_FOUND =~ false ]]; then
           echo "****************************************************" 1>&2
           echo "*** Croquis présent sur QGIS" 1>&2
-          echo "*** mais pas de fichier correspondant sur W:" 1>&2
+          echo "*** mais pas de fichier correspondant sur N:" 1>&2
           FILE_NOT_FOUND=true
     fi
     echo "${commune}: $file" 1>&2
@@ -57,7 +56,7 @@ for commune in "${COMMUNES[@]}"; do
     if [[ -z $result ]]; then
       if [[ $DB_NOT_FOUND =~ false ]]; then
         echo "***************************************" 1>&2
-        echo "*** Croquis présent sur W: mais" 1>&2
+        echo "*** Croquis présent sur N: mais" 1>&2
         echo "*** sans entrée sur QGIS" 1>&2
         DB_NOT_FOUND=true
         if [[ $DIRECTORY_DISPLAYED =~ false ]]; then
